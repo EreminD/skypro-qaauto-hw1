071b5e180041f8e3eb03e7decdb9cd47a78bb3e0
Дмитрий Еремин <mitya1705@gmail.com>
2022-04-07 16:55:47 +0300

    hw3 edited

---
+++ src/index-wait.js
@@ -0,0 +1,28 @@
+const {Capabilities,By,Builder} = require('selenium-webdriver')
+const faker = require('@faker-js/faker')
+
+async function todoApp() {
+
+    const browserName = Capabilities.chrome()
+    let driver = await new Builder().withCapabilities(browserName).build();
+
+    await driver.manage().setTimeouts({implicit: 4000})
+    await driver.get('https://sky-todo-list.herokuapp.com')
+
+    const text = faker.faker.company.catchPhrase()
+    await driver.findElement(By.css('input.input')).sendKeys(text)
+    await driver.findElement(By.css('button[type=submit]')).click()
+
+    const rows = await driver.findElements(By.css('td'))
+
+    const firstRow = rows[0]
+    await firstRow.click()
+
+    const lastRow = rows[rows.length - 1]
+    await lastRow.findElement(By.css('svg[data-icon=trash]')).click()
+
+
+    setTimeout(() => driver.quit(), 60000)
+}
+
+todoApp()
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-import { Builder, By, Capabilities} from 'selenium-webdriver'
+const {Capabilities, By, Builder} = require('selenium-webdriver')
 
 async function todoApp() {
 
-    const browserName = Capabilities.chrome()
+    const browserName = Capabilities.safari()
     let driver = await new Builder().withCapabilities(browserName).build();
 
-    /** Ваш код ниже */
-    // открыть https://sky-todo-list.herokuapp.com
+
+    await driver.get('https://sky-todo-list.herokuapp.com')
+    await driver.findElement(By.css('input.input')).sendKeys(new Date().toISOString())
+    await driver.findElement(By.css('button[type=submit]')).click()
+
+    // про ожидания мы еще не говорили
+    setTimeout(async () => {
+        const rows = await driver.findElements(By.css('td'))
+        const firstRow = rows[0]
+        firstRow.click()
+
+    }, 3000)
     
-    // добавить новый элемент в список
-
-
-    // отметить 1й элемент в списке выполненным (клик по названию)
-
-    // удалить последний элемент в списке
-    
-
-    /** Ваш код выше */
+    setTimeout(async () => {
+        const rows = await driver.findElements(By.css('td'))
+        const lastRow = rows[rows.length-1]
+        await lastRow.findElement(By.css('svg[data-icon=trash]')).click()
+    }, 5000)
+   
     setTimeout(() => driver.quit(), 60000)
 }
 
 todoApp()
-
