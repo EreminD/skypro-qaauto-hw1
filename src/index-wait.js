cc66645a1786dc1eeea761739d33b444f3ddee83
Дмитрий Еремин <mitya1705@gmail.com>
2022-04-08 11:26:37 +0300

    modified hw3

--- src/index-wait.js
+++ src/index-wait.js
@@ -1,32 +1,47 @@
 const {Capabilities,By,Builder} = require('selenium-webdriver')
-const faker = require('@faker-js/faker')
 
-async function todoApp() {
+//нужно вынести драйвер наверх, чтобы другие функции про него тоже знали
+let driver
 
+//запускаем главную функцию
+run()
+
+async function run() {
+    //сразу запускаем таймер на 10 секунд
+    setTimeout(() => driver.quit(), 10*1000)
+
+    await openToDoApp()
+    await createNewTask(`очень длинная строка для теста ${new Date()}`)
+
+    const tasks = await getTaskList()
+    await setTaskCompleted(tasks[0])
+    await deleteLastTask(tasks[tasks.length-1])
+}
+
+async function openToDoApp(){
     const browserName = Capabilities.chrome()
-    let driver = await new Builder().withCapabilities(browserName).build();
+    driver = await new Builder().withCapabilities(browserName).build();
     
     await driver.manage().setTimeouts({implicit: 4000})
-    
     await driver.manage().window().maximize()
     
     await driver.get('https://sky-todo-list.herokuapp.com')
-
-
-    await driver.findElement(By.css('input.input')).sendKeys('очень длинная строка для теста')
-    await driver.findElement(By.css('button[type=submit]')).click()
-
-    const rows = await driver.findElements(By.css('td'))
-
-    const firstRow = rows[0]
-
-    await firstRow.click()
-
-    const lastRow = rows[rows.length - 1]
-    await lastRow.findElement(By.css('svg[data-icon=trash]')).click()
-
-
-    setTimeout(() => driver.quit(), 60000)
 }
 
-todoApp()
+async function createNewTask(text){
+    await driver.findElement(By.css('input.input')).sendKeys(text)
+    await driver.findElement(By.css('button[type=submit]')).click()
+}
+
+async function getTaskList(){
+    const rows = await driver.findElements(By.css('td'))
+    return rows
+}
+
+async function setTaskCompleted(webElement){
+    await webElement.click()
+}
+
+async function deleteLastTask(webElement){
+    await webElement.findElement(By.css('svg[data-icon=trash]')).click()
+}
